2b3661e5826fad01d95c4401f421f5f3c2bffde3
Chris Smith <cleesmith2006@gmail.com>
2013-03-24 14:18:10 -0400

    sharing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,4 +51,14 @@ OscollectShare::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # for sending mail via sendmail (usually installed with postfix):
+  # FYI ... defaults to:
+  #     config.action_mailer.sendmail_settings = {
+  #       :location => '/usr/sbin/sendmail',
+  #       :arguments => '-i -t'
+  #     }  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :sendmail
+  # config.action_mailer.default_url_options = { :host => "oscollect.change_me.com" }
+  config.action_mailer.default_url_options = { :host => "oscollect.appsudo.com" }
 end
